a7ef140a354cbb31675d2afece2c22da084adf4f
Jonas Schmid <jonas.schmid@gmail.com>
2012-02-25 14:47:21 -0800

    Updated the keywords

--- neinCat.js
+++ neinCat.js
@@ -1,4 +1,4 @@
-var creepy = new Array(/cat/i, /i can has/i, /can i has/i, /kitt[en|y]/i);
+var creepy = new Array(/cat/i, /i can has/i, /can i has/i, /kitt[en|y|eh]/i, /pet/i);
 
 $$('.entry-item').each(function(el) {
 	var title = el.getProperty('data-text');
